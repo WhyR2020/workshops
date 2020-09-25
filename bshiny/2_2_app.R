12f5b2279198bf333629c72a139285fa0639b327
werpuc <puchala.weronika@gmail.com>
2020-09-25 10:18:58 +0200

    changed 2_2_app

--- bshiny/2_2_app.R
+++ bshiny/2_2_app.R
@@ -4,6 +4,10 @@ library(ggplot2)
 ui <- fluidPage(
   br(),
   sidebarPanel("Let's see the plot!",
+               checkboxGroupInput(inputId = "type_choices",
+                                  label = "Choices:",
+                                  choices = c("Births", "Deaths", "Natural_Increase"),
+                                  selected = c("Births", "Deaths", "Natural_Increase")),
                sliderInput(inputId = "x_axis_range",
                            label = "Adjust time range",
                            min = 2000, max = 2019,
@@ -18,7 +22,11 @@ server <- function(input, output){
   
   output[["nz_increase_plot"]] <- renderPlot({
     
-    ggplot(dat, aes(x = Year, y = Count, color = Type)) +
+    validate(need(input[["type_choices"]], "Please select something on the left!"))
+    
+    dat_tmp <- dat[dat[["Type"]] %in% input[["type_choices"]], ]
+    
+    ggplot(dat_tmp, aes(x = Year, y = Count, color = Type)) +
       geom_point() +
       labs(title = "The natural increase in New Zealand",
            x = "Year", 
